56c1779b35d8806e060031eaa04f269b9200c8db
Matteo Ferfoglia <matteoferfoglia@gmail.com>
2020-12-22 19:42:02 +0100

    Aggiunta classe di helper per utilizzo di OWASP encoder

---
+++ src/main/java/it/units/progrweb/utils/EncoderPrevenzioneXSS.java
@@ -0,0 +1,46 @@
+package it.units.progrweb.utils;
+
+import org.owasp.encoder.Encode;
+
+/**
+ * Classe di utilità per l'encoding col fine di difesa dagli attacchi XSS.
+ * @see Encode .
+ */
+public class EncoderPrevenzioneXSS {
+
+    /** @see org.owasp.encoder.Encode#forJava(String)   */
+    public static String encodeForJava (String input) {
+        return Encode.forJava(input);
+    }
+
+    /** @see org.owasp.encoder.Encode#forCssString(String)   */
+    public static String encodeForCSS (String input) {
+        return Encode.forCssString(input);
+    }
+
+    /** @see org.owasp.encoder.Encode#forHtml(String)  */
+    public static String encodeForHTML (String input) {
+        return Encode.forHtml(input);
+    }
+
+    /** @see org.owasp.encoder.Encode#forHtmlAttribute(String)   */
+    public static String encodeForHTMLAttribute (String input) {
+        return Encode.forHtmlAttribute(input);
+    }
+
+    /** @see org.owasp.encoder.Encode#forHtmlContent(String)   */
+    public static String encodeForHTMLContent (String input) {
+        return Encode.forHtmlContent(input);
+    }
+
+    /** @see org.owasp.encoder.Encode#forJavaScript(String)   */
+    public static String encodeForJavaScript (String input) {
+        return Encode.forJavaScript(input);
+    }
+
+    /** @see org.owasp.encoder.Encode#forJavaScriptAttribute(String)   */
+    public static String encodeForJavaScriptContainedInHTMLAttributes (String input) {
+        return Encode.forJavaScriptAttribute(input);
+    }
+
+}
