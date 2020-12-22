96148ef1d654d5f4eb4c84baf8bcc95a59c6085e
Matteo Ferfoglia <matteoferfoglia@gmail.com>
2020-12-22 19:45:39 +0100

    Bozza di servizi rest di Login e RegistrazioneNuovoConsumer

--- src/main/java/it/units/progrweb/rest/api/RegistrazioneNuovoConsumer.java
+++ src/main/java/it/units/progrweb/rest/api/RegistrazioneNuovoConsumer.java
@@ -1,5 +1,7 @@
 package it.units.progrweb.rest.api;
 
+import it.units.progrweb.utils.EncoderPrevenzioneXSS;
+
 import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
@@ -11,10 +13,52 @@ public class RegistrazioneNuovoConsumer {
 
     @POST
     @Produces(MediaType.APPLICATION_JSON)
-    @Consumes(MediaType.APPLICATION_JSON)
-    public String registrazioneNuovoConsumer(Object o) {
+    @Consumes({MediaType.APPLICATION_JSON})
+    public CampiFormRegistrazione registrazioneNuovoConsumer(CampiFormRegistrazione campiFormRegistrazione) {
         // TODO metodo e signature
-        return "Benvenuto";
+        return campiFormRegistrazione;
+    }
+}
+
+
+class CampiFormRegistrazione {
+
+    private String codiceFiscale;
+    private String nomeCognome;
+    private String email;
+    private String password;
+
+    public CampiFormRegistrazione() {}
+
+    public String getCodiceFiscale() {
+        return codiceFiscale;
     }
 
+    public String getNomeCognome() {
+        return nomeCognome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setCodiceFiscale(String codiceFiscale) {
+        this.codiceFiscale =  EncoderPrevenzioneXSS.encodeForJava(codiceFiscale);
+    }
+
+    public void setNomeCognome(String nomeCognome) {
+        this.nomeCognome = EncoderPrevenzioneXSS.encodeForJava(nomeCognome);
+    }
+
+    public void setEmail(String email) {
+        this.email = EncoderPrevenzioneXSS.encodeForJava(email);
+    }
+
+    public void setPassword(String password) {
+        this.password = EncoderPrevenzioneXSS.encodeForJava(password);
+    }
 }
