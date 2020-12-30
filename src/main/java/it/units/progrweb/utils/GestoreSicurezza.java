c71adfc91a36d8c335ba789a6c82a4d8511cba5b
Matteo Ferfoglia <matteoferfoglia@gmail.com>
2020-12-30 14:53:45 +0100

    Aggiunta una chiave segreta costante da usare nel metodo "hmacSha256" ed aggiunto il metodo "generaChiave" utilizzabile per la generazioni di chiavi crittografiche. Aggiornata di conseguenza la classe.

--- src/main/java/it/units/progrweb/utils/GestoreSicurezza.java
+++ src/main/java/it/units/progrweb/utils/GestoreSicurezza.java
@@ -20,6 +20,12 @@ public class GestoreSicurezza {
      * Vedere il parametro "algorithm" in {@link java.security.KeyPairGenerator#getInstance(String)} */
     private final static String CIPHERING_ALGORITHM = "RSA";
 
+    /** Chiave segreta usata in {@link #hmacSha256(String)},
+     * generata con algoritmo RSA a 1024 bit, vedere
+     * {@link #generaChiave(String, int)}*/  // TODO : creare variabile d'ambiente con la chiave
+    private final static String CHIAVE_SEGRETA_HS256 = "MIICeAIBADANBgkqhkiG9w0BAQEFAASCAmIwggJeAgEAAoGBAKwvp_-oaEHmBnpT4eHB-vV10CbeP0rKh4AVSoPWJvJ3_fUxNslHDBeV_G_9rS5SFn57w4TCyvIU6e2-CgcARq-nEL3bN9Oq5cA2tsXzD2YbRUfpK4_KDVexiC2NLp9SFETx_zkubOthkF5Ao3xzN7yI1FklS76q77ZRZ0ScFQrbAgMBAAECgYEApAM5CYOG286aOQeR0BOQUyOHxJ5Kt5k3fL_LHM1uh-PYWigowY0VbZoGvT5sKgUzPAPz94_89J8LPNSahJS0vqerxqIC_Yj1TAT_qFq_ncTkSqkVbz-SHy6DuUKUNTex5QLfIPCD4_VD2M-lEFfgPMTbWWcSFqjTwDBgllaeR6ECQQDfX-mO8bLqqo2RTDGab6n46iKSvMqrX25g23lmu-1KmimNzzzfwTSUv9ni-1R-R84VFDJkb42O5_xbzqZmq9OJAkEAxVXH5dglFOTjIR2ohU6X31K9rcOdhqvh6iIUTkkyEyCGZ3WYIhEC7opu4SOVa9GTA9Z3mI0BY2AL5NhXpPW-QwJBALhVy0UmWpLjam5kZW7gBXGfriZP3CRuXYVauSW5ogn1jKM1STQRmdXDOQjihYissvmcMDXIBRbQhOYydAm4dJkCQQCcc3kLwkUL_rgQjkoIfposJZZaiKeAmQ-AqEo_EwsPXQ8SQYo_IAuaAckM2EBj_gE33rZtDQYXupNTeS5ri6WvAkBfWGvoArqyepLBuWh_jGBuRDUIBJo7x4aRNIVkufRFaBDlBjWNE9eZIfaX9CCgLck5w6oNCkQmLiI092Qucryx";
+
+
     /** Lunghezza (numero di bit) delle chiavi.
      * Vedere il parametro "keysize" in {@link java.security.KeyPairGenerator#initialize(int)}.*/
     private final static int KEY_NUMBER_OF_BITS = 1024;
@@ -30,10 +36,14 @@ public class GestoreSicurezza {
     private final static GestoreSicurezza gestoreSicurezza = new GestoreSicurezza();   // TODO: da creare durante bootstrap del server
 
     private GestoreSicurezza() {
+        this(CIPHERING_ALGORITHM, KEY_NUMBER_OF_BITS);
+    }
+
+    private GestoreSicurezza(String algoritmo, int lunghezzaChiaviInNumeroDiBit) {
         try{
             // TODO : rivedere questo metodo (la funzione KeyPairGenerator.getInstance() richiederebbe anche un provider)
-            KeyPairGenerator keyGen = KeyPairGenerator.getInstance(CIPHERING_ALGORITHM);
-            keyGen.initialize(KEY_NUMBER_OF_BITS); // TODO: cosa fa questo metodo?
+            KeyPairGenerator keyGen = KeyPairGenerator.getInstance(algoritmo);
+            keyGen.initialize(lunghezzaChiaviInNumeroDiBit); // TODO: cosa fa questo metodo?
             KeyPair keyPair = keyGen.generateKeyPair();
             this.publicKey = keyPair.getPublic();
             this.privateKey = keyPair.getPrivate();
@@ -49,6 +59,12 @@ public class GestoreSicurezza {
         return gestoreSicurezza.publicKey;
     }
 
+    /** Genera una chiave crittografica, con algoritmo e lunghezza specificati nei parametri.*/
+    public static String generaChiave(String algoritmoCifratura, int lunghezzaInNumeroDiBit){
+        GestoreSicurezza gs = new GestoreSicurezza(algoritmoCifratura, lunghezzaInNumeroDiBit);
+        return Base64Helper.encodeToBase64UrlEncoded(gs.privateKey.getEncoded());
+    }
+
     /**
      * Algoritmo di hashing HMAC+SHA256, da usare per firmare i dati.
      * Fonte: https://metamug.com/article/security/jwt-java-tutorial-create-verify.html
@@ -58,7 +74,7 @@ public class GestoreSicurezza {
     public static String hmacSha256(String datiDaFirmare)
             throws NoSuchAlgorithmException, InvalidKeyException {
 
-        byte[] hash = gestoreSicurezza.privateKey.getEncoded();
+        byte[] hash = gestoreSicurezza.CHIAVE_SEGRETA_HS256.getBytes(StandardCharsets.UTF_8);
 
         Mac sha256Hmac = Mac.getInstance("HmacSHA256");
         SecretKeySpec secretKey = new SecretKeySpec(hash, "HmacSHA256");
