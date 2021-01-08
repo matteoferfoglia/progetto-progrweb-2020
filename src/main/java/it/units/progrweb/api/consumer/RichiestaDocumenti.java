a4f25a78d7006e832701344d7f7704e7173acdd4
Matteo Ferfoglia <matteoferfoglia@gmail.com>
2021-01-08 19:58:01 +0100

    Aggiunto FiltroConsumer per verificare il tipo di attore da cui proviene la richiesta ed adattamenti in RichiestaDocumenti.

--- src/main/java/it/units/progrweb/api/consumer/RichiestaDocumenti.java
+++ src/main/java/it/units/progrweb/api/consumer/RichiestaDocumenti.java
@@ -1,12 +1,11 @@
 package it.units.progrweb.api.consumer;
 
-import it.units.progrweb.entities.attori.Attore;
 import it.units.progrweb.entities.attori.Consumer;
 import it.units.progrweb.entities.file.File;
 import it.units.progrweb.utils.Autenticazione;
-import it.units.progrweb.utils.JsonHelper;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -19,7 +18,8 @@ import java.util.List;
  * Classe per rispondere alla richiesta di documenti destinati
  * ai Consumer.
  * La verifica dell'autenticazione di un attore quale Consumer
- * è demandata all'opportuno filtro.    // TODO : creare filtro per richieste destinate a /consumer
+ * è demandata all'opportuno
+ * {@link it.units.progrweb.filters.attori.FiltroConsumer filtro}.
  * @author Matteo Ferfoglia
  */
 @Path("/consumer/documenti")      // TODO : variabile d'ambiente
@@ -32,38 +32,89 @@ public class RichiestaDocumenti {
     @Path("/arrayNomiProprietaOgniDocumento")  // TODO : variabile d'ambiente
     @GET
     @Produces(MediaType.APPLICATION_JSON)       // TODO : cercare tutti i Produces e Consumes ed usare MediaType
-    public String getNomiProprietaFile() {
-        return JsonHelper.convertiOggettoInJson(File.nomiProprietaFile());
+    public String[] getNomiProprietaFile() {
+        return File.nomiProprietaFile();
     }
 
     /** Restituisce un array (JSON) in cui ogni elemento è un'astrazione
      * di un documento destinato al Consumer da cui proviene la richiesta.
      * Ogni elemento è un oggetto rappresentato come insieme di proprietà
-     * nella forma "nome-valore" (stile JavaScript) e contiene tutte e
-     * sole le proprietà date da {@link File#getProprietaFile()}.
-     * Risponde con {@link javax.ws.rs.core.Response.Status#FORBIDDEN}
-     * se il client che ha effettuato la richiesta non è un Consumer.*/
+     * nella forma "nome-valore" e contiene tutte e sole le proprietà
+     * date da {@link File#getProprietaFile()}.*/
     @Path("/elencoDocumenti")     // TODO : variabile d'ambiente
     @GET
-    public Response getElencoDocumenti(@Context HttpServletRequest httpServletRequest) {
-        Attore attore = Autenticazione.getAttoreDaHttpServletRequest(httpServletRequest);
-        if(attore instanceof Consumer) {
-            Consumer consumer = (Consumer) attore;
-            // TODO : da implementare : scaricare la lista di file di questo attore
-            List<File> listaFile = consumer.getFilesDestinatiAQuestoConsumer();
+    @Produces(MediaType.APPLICATION_JSON)
+    public String[] getElencoDocumenti(@Context HttpServletRequest httpServletRequest) {
 
-            // Ogni elemento dell'array contiene la descrizione JSON di un file:
-            String[] arrayJsonDescrizioniFile = listaFile.stream()
-                                                         .map(file -> file.toJson())
-                                                         .toArray(String[]::new);
-            String contenutoResponse = JsonHelper.convertiOggettoInJson(arrayJsonDescrizioniFile);
-            return Response.ok()
-                           .type(MediaType.APPLICATION_JSON)
-                           .entity(contenutoResponse)
-                           .build();
-        } else {
-            return Response.status(Response.Status.FORBIDDEN).build();
-        }
+        Consumer consumer = (Consumer) Autenticazione.getAttoreDaHttpServletRequest(httpServletRequest);
+        List<File> listaFile = consumer.getFilesDestinatiAQuestoConsumer();
+
+        // Ogni elemento dell'array contiene la descrizione JSON di un file:
+        String[] arrayJsonDescrizioniFile = listaFile.stream()
+                                                     .map(file -> file.toJson())
+                                                     .toArray(String[]::new);
+
+        return arrayJsonDescrizioniFile;
     }
 
+    /** Restituisce un oggetto JSON in cui ogni property
+     * è un hashtag ed il corrispettivo valore è un array
+     * contenente tutti i file (rappresentati da un
+     * identificativo). In pratica, viene restituito
+     * un indice dei file, indicizzati in base all'hashtag.
+     * Gli hashtag <strong>non</strong> sono (in generale)
+     * ordinati.
+     * Esempio (solo per illustrare questo metodo, l'esempio
+     * è scorrelato dal o dal linguaggio di programmazione).
+     * Si consideri il seguente array di valori proveniente
+     * dalla richiesta del client:
+     * <code>[25,27,9]</code>.
+     * Tali valori corrispono a dei file secondo una mappatura
+     * nota al server e siano le seguenti le rappresentazioni
+     * dei file associati agli identificatori ottenuti dalla
+     * richiesta del client:
+     * <ol>
+     *     <li><code>{"identificativo": 25, "nomeFile": "Fattura123", "hashtags": ["fattura","pagare","soldi"]}</code></li>
+     *     <li><code>{"identificativo": 27, "nomeFile": "PreventivoA26", "hashtags": ["pagare"]}</code></li>
+     *     <li><code>{"identificativo": 9, "nomeFile": "Bonifico", "hashtags": ["guadagno","soldi"]}</code></li>
+     * </ol>
+     * Allora, questo metodo restituirà (a meno dell'ordine
+     * delle property, oppure dei valori numerici -
+     * <strong>non</strong> necessariamente ordinati) il
+     * seguente oggetto JSON:
+     * <pre><code>
+     *     {
+     *         "fattura":[25],
+     *         "pagare":[25,27],
+     *         "soldi":[25,9],
+     *         "guadagno":[9]
+     *     }
+     * </code></pre>
+     *
+     * @param identificativiFile Array degli identificativi dei file
+     *                           per i quali si vuole l'indicizzazione
+     *                           rispetto agli hashtag.
+     * @return L'indice dei file, indicizzati in base agli hashtag
+     *          che essi contengono.
+     */
+    @GET
+    @Path("/{arrayIdentificativiFile}")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getListaHashtags(long[] identificativiFile) {
+
+        // TODO : rivedere questo metodo e la sua descrizione
+
+        // TODO : testare : deserializzazione corretta ??? Meglio usare @PathParam ? @QueryParam ?
+
+        return rispostaOkContenutoJson(""); // TODO
+    }
+
+    /** Costruisce una risposta, con contenuto in formato JSON.*/
+    private static Response rispostaOkContenutoJson(String contenutoJsonResponse) {
+        return Response.ok()
+                       .type(MediaType.APPLICATION_JSON)
+                       .entity(contenutoJsonResponse)
+                       .build();
+    }
 }
---
+++ src/main/java/it/units/progrweb/filters/attori/FiltroConsumer.java
@@ -0,0 +1,37 @@
+package it.units.progrweb.filters.attori;
+
+import it.units.progrweb.entities.attori.Attore;
+import it.units.progrweb.entities.attori.Consumer;
+import it.units.progrweb.utils.Autenticazione;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Intercetta le richieste per i Consumer e verifica
+ * che siano tali (e non ad esempio Uploader).
+ * @author Matteo Ferfoglia
+ */
+@WebFilter(filterName = "FiltroConsumer", asyncSupported = true, urlPatterns = {"/api/consumer/*"})  // TODO : url pattern variabile d'ambiente
+public class FiltroConsumer implements Filter {
+
+    public void destroy() {}
+
+    public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain)
+            throws ServletException, IOException {
+
+        if(req instanceof HttpServletRequest) { // TODO : filtro che intercetta tutte le richieste e verifica se sono HttpServletRequest
+            Attore attore = Autenticazione.getAttoreDaHttpServletRequest((HttpServletRequest) req);
+            if(attore instanceof Consumer)
+                chain.doFilter(req, resp);
+        } else {
+            Autenticazione.rispondiNonAutorizzato((HttpServletResponse) resp);
+        }
+    }
+
+    public void init(FilterConfig config) throws ServletException {}
+
+}
