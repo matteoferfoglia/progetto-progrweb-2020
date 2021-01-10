70ecb6fc5c66ea8e3b7ea8062b1d0ed64a71402e
Matteo Ferfoglia <matteoferfoglia@gmail.com>
2021-01-11 00:45:22 +0100

    Modificati i servizi di Login/Logout ad accesso programmatico ed aggiunto servizio per verificare se il client che ha effettuato la richiesta è autenticato.

--- src/main/java/it/units/progrweb/api/LoginLogout.java
+++ src/main/java/it/units/progrweb/api/LoginLogout.java
@@ -1,28 +1,52 @@
 package it.units.progrweb.api;
 
-import it.units.progrweb.utils.Autenticazione;
 import it.units.progrweb.utils.EncoderPrevenzioneXSS;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.*;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 /**
- * Classe per la gestione del login dei client.
+ * Classe per la gestione dell'autenticazione dei client.
  * @author Matteo Ferfoglia
  */
-@Path("/login")     // TODO : path variabile d'ambiente
-public class Login {
+@Path("")
+public class LoginLogout {
 
     /** Risponde alle richieste di login, rilasciando
      * un'opportuna {@link Response} al client.*/
+    @Path("/login")     // TODO : path variabile d'ambiente
     @POST
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.TEXT_PLAIN)
     public Response login(CampiFormLogin campiFormLogin){
 
         // TODO : verificare che CSRF token venga controllato e testare
-        return Autenticazione.creaResponseAutenticazione(campiFormLogin.getUsername(), campiFormLogin.getPassword());
+        return it.units.progrweb.utils.Autenticazione.creaResponseAutenticazione(campiFormLogin.getUsername(), campiFormLogin.getPassword());
+    }
+
+    /** Effettua il logout del client che ne fa richiesta. */
+    @Path("/logout")     // TODO : path variabile d'ambiente
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public Response logout() {
+
+        // TODO : testare
+        return it.units.progrweb.utils.Autenticazione.creaResponseLogout();
+
+    }
+
+    /** Servizio per verificare se l'utente che ha effettuato
+     * la richiesta risulta attualmente autenticato: se la
+     * verifica ha esito positivo, questo metodo restituisce
+     * "true", altrimenti "false".*/
+    @Path("/verificaTokenAutenticazione")   // TODO : var d'ambiente
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public boolean isClientAttualmenteAutenticato(@Context HttpServletRequest httpServletRequest) {
+        return it.units.progrweb.utils.Autenticazione.isClientAutenticato(httpServletRequest);
     }
 
 }
