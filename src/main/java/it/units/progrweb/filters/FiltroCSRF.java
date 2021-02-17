11bb93a183f6d2bdd2252364bd063cad162c84da
Matteo Ferfoglia <matteoferfoglia@gmail.com>
2021-02-17 21:31:51 +0100

    FIX.
    La classe HttpRequestWrapper faceva uso del metodo toString() e considerava il payload come String, ma in realtà esso può essere in generale un array di byte (byte[]), ad esempio se si esegue l'upload di un file binario. Ora il problema è risolto e la classe in questione utilizza byte[] e non String.

--- src/main/java/it/units/progrweb/filters/FiltroCSRF.java
+++ src/main/java/it/units/progrweb/filters/FiltroCSRF.java
@@ -202,46 +202,43 @@ class HttpRequestWrapper extends HttpServletRequestWrapper {
 
     // TODO : classe da rivedere
 
-    private final String payload;
+    private final byte[] payload;
 
     public HttpRequestWrapper(HttpServletRequest request) throws IOException {
         super(request);
 
         // read the original payload into the payload variable
-        StringBuilder stringBuilder = new StringBuilder();
-        BufferedReader bufferedReader = null;
+        ByteArrayOutputStream builder = new ByteArrayOutputStream();
+        InputStream inputStream = request.getInputStream();
         try {
-            // read the payload into the StringBuilder
-            InputStream inputStream = request.getInputStream();
             if (inputStream != null) {
-                bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
-                char[] charBuffer = new char[128];
+                byte[] buffer = new byte[128];
                 int bytesRead = -1;
-                while ((bytesRead = bufferedReader.read(charBuffer)) > 0) {
-                    stringBuilder.append(charBuffer, 0, bytesRead);
+                while ((bytesRead = inputStream.read(buffer, 0, buffer.length)) > 0) {
+                    builder.write(buffer, 0, bytesRead);
                 }
             } else {
                 // make an empty string since there is no payload
-                stringBuilder.append("");
+                builder.write(Byte.valueOf(""));
             }
         } catch (IOException ex) {
             Logger.scriviEccezioneNelLog(HttpRequestWrapper.class, "Error reading the request payload", ex);
             throw new IOException("Error reading the request payload", ex);
         } finally {
-            if (bufferedReader != null) {
+            if (inputStream != null) {
                 try {
-                    bufferedReader.close();
+                    inputStream.close();
                 } catch (IOException iox) {
                     // ignore
                 }
             }
         }
-        payload = stringBuilder.toString();
+        payload = builder.toByteArray();
     }
 
     @Override
     public ServletInputStream getInputStream () throws IOException {
-        final ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(payload.getBytes());
+        final ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(payload);
         ServletInputStream inputStream = new ServletInputStream() {
             @Override
             public boolean isFinished() {return false;}
