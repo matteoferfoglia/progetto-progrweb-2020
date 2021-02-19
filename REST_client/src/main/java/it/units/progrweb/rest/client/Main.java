3fd52649831993f53194ef495359b8f948e706e4
Matteo Ferfoglia <matteoferfoglia@gmail.com>
2021-02-19 19:21:44 +0100

    Aggiunte credenziali dell'Uploader nella richiesta del client rest. Ora si utilizza bodyPart per l'invio del file. Ristrutturazione del main.

--- REST_client/src/main/java/it/units/progrweb/rest/client/Main.java
+++ REST_client/src/main/java/it/units/progrweb/rest/client/Main.java
@@ -7,25 +7,43 @@ import java.net.URL;
 import java.util.Scanner;
 
 /**
+ * Classe main per dimostrare il funzionamento del web service: un
+ * Uploader può usare il web service esposta dal sistema per caricare
+ * tramite questa applicazione client un documento destinato ad un
+ * Consumer. L'uploader deve specificare qui le proprie credenziali
+ * ed i parametri per il file da caricare, come richiesti dal web
+ * service.
  * @author Matteo Ferfoglia
  */
 public class Main {
 
     public static void main(String[] args) {
+
+        // Uri web service
+        String uri_webService = "http://localhost:8910/api/webService/uploadDocumento";
+
+        // Credenziali uploader
+        String credenziali_username = "AB01";
+        String credenziali_password = "5678";
+
         // Parametri per l'invio dei file
         String codiceFiscaleConsumer = "PPPPLT80A01A952G";
         String emailConsumer = "pippopluto@example.com";
         String nomeCognomeConsumer = "Pippo Pluto";
         String nomeFile = "file prova";
         String listaHashtag = "rest, file, prova, primo file";
-        File file;  // da inizializzare
+        String percorsoFileLocale = "fileProva.txt";
 
+
+        // ---- FINE PARAMETRI ----
+
+
+        File file;  // da inizializzare
         {
             // Lettura del file
-            String nomeFileLocale = "fileProva.txt";
-            URL urlFileLocale = Main.class.getClassLoader().getResource(nomeFileLocale);
+            URL urlFileLocale = Main.class.getClassLoader().getResource(percorsoFileLocale);
             if (urlFileLocale == null) {
-                throw new IllegalArgumentException("File " + nomeFileLocale + " non trovato.");
+                throw new IllegalArgumentException("File " + percorsoFileLocale + " non trovato.");
             } else {
                 try {
                     file = new File(urlFileLocale.toURI());
@@ -37,9 +55,22 @@ public class Main {
             }
         }
 
+        // Creazione client REST ed invio del contenuto
+        RestClient restClient = new RestClient(uri_webService);
+        try {
 
-        RestClient restClient = new RestClient("http://localhost:8910/api/nomeDiQuestoAttore", "AB01", "5678");
-        restClient.inviaFileAConsumer(codiceFiscaleConsumer, emailConsumer, nomeCognomeConsumer, nomeFile, listaHashtag, file);
+            String responseBody =
+                    restClient.inviaFileAConsumer(credenziali_username, credenziali_password,
+                                                  codiceFiscaleConsumer, emailConsumer, nomeCognomeConsumer,
+                                                  nomeFile, listaHashtag, file)
+                              .readEntity(String.class);
+            System.out.println("--- RISPOSTA DAL SERVER ---\n" + responseBody);
+
+        } catch (Exception e) {
+            System.err.println("Errore durante l'invio:\n\n" + e.getMessage() + "\n\n\tSTACK TRACE:\n");
+            e.printStackTrace(System.err);
+        }
+
     }
 
 
--- REST_client/src/main/java/it/units/progrweb/rest/client/RestClient.java
+++ REST_client/src/main/java/it/units/progrweb/rest/client/RestClient.java
@@ -1,6 +1,5 @@
 package it.units.progrweb.rest.client;
 
-import com.sun.org.apache.bcel.internal.generic.RET;
 import org.glassfish.jersey.media.multipart.FormDataMultiPart;
 import org.glassfish.jersey.media.multipart.MultiPartFeature;
 import org.glassfish.jersey.media.multipart.file.FileDataBodyPart;
@@ -11,10 +10,6 @@ import javax.ws.rs.client.Entity;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.io.File;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.util.Arrays;
-import java.util.List;
 
 /**
  * @author Matteo Ferfoglia
@@ -27,46 +22,51 @@ public class RestClient {
     /** Istanza del client.*/
     private final Client client;
 
-    public RestClient(String webServiceUri, String username, String password) {
+    public RestClient(String webServiceUri) {
         this.restWebServiceUri = webServiceUri;
         this.client =  ClientBuilder.newClient();
-        this.client.property("username", username)
-                   .property("password", password)
-                   .register(MultiPartFeature.class);
+        this.client.register(MultiPartFeature.class);
     }
 
-    public Response inviaFileAConsumer(String codiceFiscaleConsumer, String emailConsumer, String nomeCognomeConsumer,
+    public Response inviaFileAConsumer(String usernameUploader, String passwordUploader,
+                                       String codiceFiscaleConsumer, String emailConsumer, String nomeCognomeConsumer,
                                        String nomeFile, String listaHashtag, File file) {
 
-        FileDaCaricare fileDaCaricare = new FileDaCaricare(codiceFiscaleConsumer, emailConsumer, nomeCognomeConsumer,
+        FileDaCaricare fileDaCaricare = new FileDaCaricare(usernameUploader, passwordUploader,
+                                                           codiceFiscaleConsumer, emailConsumer, nomeCognomeConsumer,
                                                            nomeFile, listaHashtag, file);
 
         return client.target(restWebServiceUri)
-                     .request(MediaType.TEXT_PLAIN)
+                     .request(MediaType.APPLICATION_JSON)
                      .post(Entity.entity(fileDaCaricare.getFormDataMultiPart(), fileDaCaricare.getFormDataMultiPart().getMediaType()));
 
     }
-    
+
 }
 
 /** Classe di supporto rappresentante un file da caricare, con tutti
  * gli attributi richiesti dal web service.*/
 class FileDaCaricare {
 
+    private final String usernameUploader;
+    private final String passwordUploader;
     private final String codiceFiscaleConsumer;
     private final String emailConsumer;
     private final String nomeCognomeConsumer;
     private final String nomeFile;
-    private final List<String> listaHashtag;
+    private final String listaHashtag;
     private final File file;
 
-    public FileDaCaricare(String codiceFiscaleConsumer, String emailConsumer, String nomeCognomeConsumer,
+    public FileDaCaricare(String usernameUploader, String passwordUploader,
+                          String codiceFiscaleConsumer, String emailConsumer, String nomeCognomeConsumer,
                           String nomeFile, String listaHashtag, File file) {
+        this.usernameUploader = usernameUploader;
+        this.passwordUploader = passwordUploader;
         this.codiceFiscaleConsumer = codiceFiscaleConsumer;
         this.emailConsumer = emailConsumer;
         this.nomeCognomeConsumer = nomeCognomeConsumer;
         this.nomeFile = nomeFile;
-        this.listaHashtag = Arrays.asList(listaHashtag.split(", "));
+        this.listaHashtag = listaHashtag;
         this.file = file;
     }
 
@@ -76,13 +76,17 @@ class FileDaCaricare {
     FormDataMultiPart getFormDataMultiPart() {
 
         FormDataMultiPart formDataMultiPart = new FormDataMultiPart();
+        FileDataBodyPart fileDataBodyPart = new FileDataBodyPart("contenutoFile", file,
+                                                        MediaType.APPLICATION_OCTET_STREAM_TYPE);
 
-        formDataMultiPart.field("codiceFiscale", codiceFiscaleConsumer);
-        formDataMultiPart.field("emailConsumer", emailConsumer);
-        formDataMultiPart.field("nomeCognomeConsumer", nomeCognomeConsumer);
-        formDataMultiPart.field("nomeFile", nomeFile);
-        formDataMultiPart.field("listaHashtag", listaHashtag, MediaType.APPLICATION_JSON_TYPE);
-        formDataMultiPart.field("file", file, MediaType.APPLICATION_OCTET_STREAM_TYPE);
+        formDataMultiPart.field("usernameUploader", usernameUploader);
+        formDataMultiPart.field("passwordUploader", passwordUploader);
+        formDataMultiPart.field("codiceFiscaleConsumerDestinatario", codiceFiscaleConsumer);
+        formDataMultiPart.field("emailConsumerDestinatario",         emailConsumer);
+        formDataMultiPart.field("nomeCognomeConsumerDestinatario",   nomeCognomeConsumer);
+        formDataMultiPart.field("nomeFile",     nomeFile);
+        formDataMultiPart.field("listaHashtag", listaHashtag);
+        formDataMultiPart.bodyPart(fileDataBodyPart);
 
         return formDataMultiPart;
 
