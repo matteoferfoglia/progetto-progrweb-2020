68371f5c6584f5d8be93b02f3eb92d4540ba7ab1
Matteo Ferfoglia <matteoferfoglia@gmail.com>
2021-02-23 09:43:24 +0100

    Icone svg spostate in content css

--- Progetto_VUE/progetto-progrweb-2020-vue/src/components/attori/SchedaDiUnAttore.vue
+++ Progetto_VUE/progetto-progrweb-2020-vue/src/components/attori/SchedaDiUnAttore.vue
@@ -1,7 +1,5 @@
 <template>
 
-  <!-- TODO : rivedere e semplificare questa scheda -->
-
   <header>
     <img :src="logoBase64_dataUrl"
          alt=""
@@ -32,34 +30,23 @@
                        @csrf-token-ricevuto="$emit('csrf-token-ricevuto',$event)">
 
         <input type="file" :name="LOGO_INPUT_FIELD_NAME"
-               v-if="mostrareInputFilePerModificaLogoUploader()" ><!-- TODO : testare la modifica del logo -->
+               v-if="mostrareInputFilePerModificaLogoUploader()" >
 
         <button @click="modificaAttore()"
+                class="modifica"
                 v-if="! isConsumerAttualmenteAutenticato()">
-          <!-- Fonte icona: https://icons.getbootstrap.com/icons/pen/ --><!-- TODO : icone da aggiungere via CSS -->
-          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pen" viewBox="0 0 16 16">
-            <path d="M13.498.795l.149-.149a1.207 1.207 0 1 1 1.707 1.708l-.149.148a1.5 1.5 0 0 1-.059 2.059L4.854 14.854a.5.5 0 0 1-.233.131l-4 1a.5.5 0 0 1-.606-.606l1-4a.5.5 0 0 1 .131-.232l9.642-9.642a.5.5 0 0 0-.642.056L6.854 4.854a.5.5 0 1 1-.708-.708L9.44.854A1.5 1.5 0 0 1 11.5.796a1.5 1.5 0 0 1 1.998-.001zm-.644.766a.5.5 0 0 0-.707 0L1.95 11.756l-.764 3.057 3.057-.764L14.44 3.854a.5.5 0 0 0 0-.708l-1.585-1.585z"/>
-          </svg>
           Modifica utente
         </button>
 
         <button @click.prevent="eliminaAttore()"
+                class="x-circle"
                 v-if="! isConsumerAttualmenteAutenticato()">
-          <!-- Fonte icona: https://icons.getbootstrap.com/icons/x-circle/ --><!-- TODO : icone da aggiungere via CSS -->
-          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x-circle" viewBox="0 0 16 16">
-            <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
-            <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>
-          </svg>
           Elimina utente
         </button>
 
         <button @click.prevent="ripristinaValoriProperty = true"
+                class="reset"
                 v-if="! isConsumerAttualmenteAutenticato()">
-          <!-- Fonte icona: https://icons.getbootstrap.com/icons/arrow-counterclockwise/ --><!-- TODO : icone da aggiungere via CSS -->
-          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-counterclockwise" viewBox="0 0 16 16">
-            <path fill-rule="evenodd" d="M8 3a5 5 0 1 1-4.546 2.914.5.5 0 0 0-.908-.417A6 6 0 1 0 8 2v1z"/>
-            <path d="M8 4.466V.534a.25.25 0 0 0-.41-.192L5.23 2.308a.25.25 0 0 0 0 .384l2.36 1.966A.25.25 0 0 0 8 4.466z"/>
-          </svg>
           Reset modifiche
         </button>
 
@@ -86,12 +73,8 @@
 
 
   <button @click="chiudiSchedaAttore"
+          class="x-circle"
           v-if="mostrarePulsanteChiusuraQuestaSchedaAttore===true" >
-    <!-- Fonte (icona): https://icons.getbootstrap.com/icons/x-circle/ --><!-- TODO : mettere icona in CSS -->
-    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x-circle" viewBox="0 0 16 16">
-      <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
-      <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>
-    </svg>
     Chiudi
   </button>
 
@@ -454,4 +437,16 @@ name: "SchedaDiUnAttore",
 </script>
 
 <style scoped>
+  button.modifica::before {
+    /* Fonte icona: https://icons.getbootstrap.com/icons/pen/ */
+    content: url('data:image/svg+xml; utf8, <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pen" viewBox="0 0 16 16"><path d="M13.498.795l.149-.149a1.207 1.207 0 1 1 1.707 1.708l-.149.148a1.5 1.5 0 0 1-.059 2.059L4.854 14.854a.5.5 0 0 1-.233.131l-4 1a.5.5 0 0 1-.606-.606l1-4a.5.5 0 0 1 .131-.232l9.642-9.642a.5.5 0 0 0-.642.056L6.854 4.854a.5.5 0 1 1-.708-.708L9.44.854A1.5 1.5 0 0 1 11.5.796a1.5 1.5 0 0 1 1.998-.001zm-.644.766a.5.5 0 0 0-.707 0L1.95 11.756l-.764 3.057 3.057-.764L14.44 3.854a.5.5 0 0 0 0-.708l-1.585-1.585z"/></svg>');
+  }
+  button.x-circle::before {
+    /* Fonte (icona): https://icons.getbootstrap.com/icons/x-circle/ */
+    content: url('data:image/svg+xml; utf8, <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pen" viewBox="0 0 16 16"><path d="M13.498.795l.149-.149a1.207 1.207 0 1 1 1.707 1.708l-.149.148a1.5 1.5 0 0 1-.059 2.059L4.854 14.854a.5.5 0 0 1-.233.131l-4 1a.5.5 0 0 1-.606-.606l1-4a.5.5 0 0 1 .131-.232l9.642-9.642a.5.5 0 0 0-.642.056L6.854 4.854a.5.5 0 1 1-.708-.708L9.44.854A1.5 1.5 0 0 1 11.5.796a1.5 1.5 0 0 1 1.998-.001zm-.644.766a.5.5 0 0 0-.707 0L1.95 11.756l-.764 3.057 3.057-.764L14.44 3.854a.5.5 0 0 0 0-.708l-1.585-1.585z"/></svg>');
+  }
+  button.reset::before {
+    /* Fonte icona: https://icons.getbootstrap.com/icons/arrow-counterclockwise/ */
+    content: url('data:image/svg+xml; utf8, <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x-circle" viewBox="0 0 16 16"><path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/><path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/></svg>');
+  }
 </style>
