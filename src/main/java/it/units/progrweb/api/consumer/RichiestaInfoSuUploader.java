1264b36b7b7899f94d3229f6ae885bb9f81fe267
Matteo Ferfoglia <matteoferfoglia@gmail.com>
2021-03-02 14:42:50 +0100

    Rimosso metodo non utilizzato.

--- src/main/java/it/units/progrweb/api/consumer/RichiestaInfoSuUploader.java
+++ src/main/java/it/units/progrweb/api/consumer/RichiestaInfoSuUploader.java
@@ -4,7 +4,6 @@ import it.units.progrweb.entities.attori.nonAdministrator.consumer.Consumer;
 import it.units.progrweb.entities.attori.nonAdministrator.uploader.Uploader;
 import it.units.progrweb.entities.file.File;
 import it.units.progrweb.utils.Autenticazione;
-import it.units.progrweb.utils.UtilitaGenerale;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.GET;
@@ -12,9 +11,6 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
-import java.util.List;
-import java.util.Map;
 
 /**
  * Servizi per richiedere le informazioni sugli Uploader.
@@ -38,41 +34,6 @@ public class RichiestaInfoSuUploader {
                 Autenticazione.getIdentificativoAttoreDaTokenAutenticazione(httpServletRequest);
 
         return File.getElencoUploaderServentiConsumer(identificativoQuestoConsumer);
-
-    }
-
-
-    /** In base all'header della richiesta, capisce da quale
-     * Consumer essa provenga, quindi cerca tutti gli Uploader
-     * che hanno caricato almeno un documento per quel Consumer
-     * e li restituisce in un oggetto JSON in cui ogni property
-     * ha come nome l'identificativo dell'Uploader e come
-     * valore l'array contenente gli identificativi di tutti i
-     * file caricati dall'Uploader identificato dal nome della
-     * property e destinati al Consumer da cui proviene la
-     * richiesta.*/
-    @Path("/mappaId-uploader-files")        // TODO : variabile d'ambiente
-    @GET
-    // Response costruita senza @Produces per serializzare i dati in modo personalizzato
-    public Response getMappa_idUploader_fileInviatiAlConsumer(@Context HttpServletRequest httpServletRequest) {
-
-        // TODO : probabilmente questo servizio è da eliminare
-
-        // TODO : verificare correttezza
-        // TODO : rivedere : che cosa va qua e che cosa va nel metodo che restituisce l'elenco degli uploader
-
-        Consumer consumer = (Consumer) Autenticazione.getAttoreDaHttpServletRequest(httpServletRequest);
-        Map<Long, Long[]> mappa_idUploader_arrayIdFileCaricatiDaUploaderPerQuestoConsumer = null;
-
-        if ( consumer != null) {
-            List<File> risultatoQuery = File.getOccorrenzeFiltratePerConsumer(consumer.getIdentificativoAttore());
-
-           mappa_idUploader_arrayIdFileCaricatiDaUploaderPerQuestoConsumer =
-                    File.mappa_identificativoUploader_arrayIdFile(risultatoQuery);
-
-        }
-
-        return UtilitaGenerale.rispostaJsonConMappa(mappa_idUploader_arrayIdFileCaricatiDaUploaderPerQuestoConsumer);
 
     }
 
