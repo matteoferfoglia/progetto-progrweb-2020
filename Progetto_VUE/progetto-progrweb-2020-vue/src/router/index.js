6e3cad14faa558ffacb1a28a19eabf4be0dd42e2
Matteo Ferfoglia <matteoferfoglia@gmail.com>
2021-03-07 15:26:10 +0100

    Ritornati all'utilizzo di createWebHashHistory per evitare l'aggiornamento della pagina.

--- Progetto_VUE/progetto-progrweb-2020-vue/src/router/index.js
+++ Progetto_VUE/progetto-progrweb-2020-vue/src/router/index.js
@@ -14,7 +14,7 @@
 
 // TODO : rivedere ed eventualmente ristrutturare questo script
 
-import {createRouter, createWebHistory} from 'vue-router'
+import {createRouter, createWebHashHistory} from 'vue-router'
 import {eliminaTokenAutenticazione, verificaAutenticazione} from "../utils/autenticazione";
 import {getProprietaAttoreTarget, getTipoAttoreTarget} from "../utils/richiesteInfoSuAttori";
 import {rimuoviAuthorizationHeader} from "../utils/http";
@@ -124,7 +124,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes: routes
 });
 
