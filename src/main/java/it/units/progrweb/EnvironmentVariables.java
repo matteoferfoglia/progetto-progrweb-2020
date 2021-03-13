1c81b6651c9669e5755267ed02d6a9c1d4af004b
Matteo Ferfoglia <matteoferfoglia@gmail.com>
2021-03-13 20:04:28 +0100

    Creata una classe per memorizzare le variabili d'ambiente.

---
+++ src/main/java/it/units/progrweb/EnvironmentVariables.java
@@ -0,0 +1,15 @@
+package it.units.progrweb;
+
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+
+/**
+ * Classe in cui sono definite tutte le variabili d'ambiente del progetto.
+ * @author Matteo Ferfoglia
+ */
+public class EnvironmentVariables {
+
+    /** Standard charset utilizzato nel progetto. */
+    public static final Charset STANDARD_CHARSET = StandardCharsets.UTF_8;
+
+}
