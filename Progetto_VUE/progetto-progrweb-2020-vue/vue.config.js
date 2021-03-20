acc52102cbbe02c2b3574be174873ce634ab5f3f
Matteo Ferfoglia <matteoferfoglia@gmail.com>
2021-03-20 08:29:10 +0100

    Ora, in production non viene creata la source map (*.js.map).

---
+++ Progetto_VUE/progetto-progrweb-2020-vue/vue.config.js
@@ -0,0 +1,12 @@
+// vue.config.js
+
+/* vue.config.js is an optional config file that will be automatically
+ * loaded by @vue/cli-service if it's present in your project root (next
+ * to package.json). You can also use the vue field in package.json, but
+ * do note in that case you will be limited to JSON-compatible values only.
+ *
+ * Fonte: https://cli.vuejs.org/config/#global-cli-config */
+
+module.exports = {
+    productionSourceMap: false  // evita source maps in production
+}
