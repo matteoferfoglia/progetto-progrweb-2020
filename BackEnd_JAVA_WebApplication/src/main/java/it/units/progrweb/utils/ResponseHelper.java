289d00bb01c1bda19f60ac14bb8b559efaee8f67
Matteo Ferfoglia <matteoferfoglia@gmail.com>
2021-04-15 10:19:20 +0200

    Risolti warning Javadoc.

--- BackEnd_JAVA_WebApplication/src/main/java/it/units/progrweb/utils/ResponseHelper.java
+++ BackEnd_JAVA_WebApplication/src/main/java/it/units/progrweb/utils/ResponseHelper.java
@@ -4,17 +4,14 @@ import javax.servlet.http.HttpServletResponse;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.io.IOException;
-import java.lang.reflect.Constructor;
-import java.lang.reflect.InvocationTargetException;
 import java.net.URI;
 
 /**
  * Classe con metodi di utilità che restituiscono degli
  * oggetti di tipo {@link javax.ws.rs.core.Response} con
- * {@link Response.Status} intuibile dal nome dei metodi
- * e messaggio (entity) atteso come parametro. Potrebbero
- * essere richiesti ulteriori parametri intuibili dalla
- * firma del metodo.
+ * Status intuibile dal nome dei metodi e messaggio (entity)
+ * atteso come parametro. Potrebbero essere richiesti ulteriori
+ * parametri intuibili dalla firma del metodo.
  *
  * @author Matteo Ferfoglia
  */
@@ -119,26 +116,26 @@ public class ResponseHelper {
 
     // Helper
 
-    /** Dati messaggio {@link Response.Status} e cookie, restituisce l'oggetto
+    /** Dati messaggio  Response.Status e cookie, restituisce l'oggetto
      * {@link Response} corrispondente, con {@link MediaType} {@link MediaType#TEXT_PLAIN}.*/
     private static<E> Response creaResponseText(E messaggio, Response.Status status, Cookie[] cookies, HeaderResponse headerResponse) {
         return creaResponseMediaTypePersonalizzato(messaggio, status, MediaType.TEXT_PLAIN_TYPE, headerResponse, cookies);
     }
 
-    /** Dati messaggio e {@link Response.Status}, restituisce l'oggetto
+    /** Dati messaggio e  Response.Status, restituisce l'oggetto
      * {@link Response} corrispondente, con {@link MediaType} {@link MediaType#TEXT_PLAIN}.*/
     private static<E> Response creaResponseText(E messaggio, Response.Status status, HeaderResponse headerResponse) {
         return creaResponseText(messaggio, status, new Cookie[0], headerResponse);
     }
 
-    /** Dati messaggio e {@link Response.Status}, restituisce l'oggetto
+    /** Dati messaggio e  Response.Status, restituisce l'oggetto
      * {@link Response} corrispondente, con {@link MediaType} {@link MediaType#TEXT_PLAIN}.*/
     private static<E> Response creaResponseText(E messaggio, Response.Status status) {
         return creaResponseText(messaggio, status, null);
     }
 
 
-    /** Dati messaggio, {@link Response.Status}, {@link MediaType}, {@link HeaderResponse} e {@link Cookie Cookie[]},
+    /** Dati messaggio,  Response.Status, {@link MediaType}, {@link HeaderResponse} e {@link Cookie Cookie[]},
      * restituisce l'oggetto {@link Response} corrispondente, con {@link MediaType} specificato.*/
     private static<E> Response creaResponseMediaTypePersonalizzato(E messaggio, Response.Status status, MediaType mediaType, HeaderResponse headerResponse, Cookie[] cookies) {
         Response.ResponseBuilder tmp = creaResponse_builder(messaggio, status, mediaType);
@@ -153,8 +150,8 @@ public class ResponseHelper {
         return tmp;
     }
 
-    /** Dati messaggio e {@link Response.Status}, restituisce l'oggetto
-     * {@link Response.ResponseBuilder} corrispondente, impostando i campi
+    /** Dati messaggio e Status, restituisce l'oggetto
+     * ResponseBuilder corrispondente, impostando i campi
      * "entity", "type" e "status".*/
     private static<E> Response.ResponseBuilder creaResponse_builder(E messaggio, Response.Status status, MediaType mediaType) {
         return Response.status( status ).type( mediaType ).entity( messaggio );
